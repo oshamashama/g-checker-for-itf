ae09c18eb52d4a50411a42ebe5ac1e92825a5c08
oshamashama <65126083+oshamashama@users.noreply.github.com>
2022-02-05 03:13:15 +0900

    add file upload

--- src/App.js
+++ src/App.js
@@ -1,95 +1,172 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import Data from './grade.json'
+import Data from './reqJson/coins20.json'
 import { useTable } from "react-table";
-
+import { readCsv } from './checkGraduate/readCsv.ts'
+import { checkGraduate } from './checkGraduate/checkGrad.ts'
+import Select from 'react-select'
 
 
 class Table extends React.Component {
-    render() {
-        let itemList = []
-        for (const key in this.props.value) {
-            let pushStr = ' ' + (this.props.value[key]['now_certificated_credit_num'])
-                + '('
-                + this.props.value[key]['feature_certificated_credit_num']
-                + ')/'
-                + (this.props.value[key]['min_certificated_credit_num'])
-            let passed = ""
-            if (this.props.value[key]['now_certificated_credit_num'] >= this.props.value[key]['min_certificated_credit_num'])
-                passed = "passed"
-            else if (this.props.value[key]['now_certificated_credit_num'] + this.props.value[key]['feature_certificated_credit_num'] >= this.props.value[key]['min_certificated_credit_num'])
-                passed = "expect"
-            else
-                passed = "failed"
-            itemList.push([key, pushStr, passed])
-        }
-        if (this.props.depth === 0) {
-            return (
-                <div className='con'>
-                    {itemList.map(([key, st, passed]) => (
-                        <div className={'bl_tate ' + passed} key={key + st}>
-                            <p className='hight-center last-leaf'>
-                                {(key === "" ? "" : key) + st}
-                            </p>
-                        </div>
-                    ))}
-                </div>
-            )
-        }
-
-        return (
-            <div className='con'>
-                {itemList.map(([key, st, passed]) => (
-                    <div className={'gre_yoko ' + passed} key={key + st}>
-                        <div className={'bl_tate ' + passed} key={key + st + 'top'}>
-                            <p className='hight-center'>
-                                {(key === "" ? "" : key)}
-                            </p>
-                            <p className='hight-center'>
-                                {st}
-                            </p>
-                        </div>
-                        <div className={'bl_tate2 ' + passed} key={key + st}>
-                            <Table value={this.props.value[key]['leaf']} depth={this.props.depth - 1} />
-                        </div>
-                    </div>
-                ))}
-            </div>
-        )
+  render() {
+    console.log("updateupdateupdateupdateupdateupdateupdateupdateupdateupdateupdateupdateupdate");
+    let itemList = []
+    for (const key in this.props.value) {
+      let pushStr = ' ' + (this.props.value[key]['now_certificated_credit_num'])
+        + '('
+        + this.props.value[key]['feature_certificated_credit_num']
+        + ')/'
+        + (this.props.value[key]['min_certificated_credit_num'])
+      let passed = ""
+      if (this.props.value[key]['now_certificated_credit_num'] >= this.props.value[key]['min_certificated_credit_num'])
+        passed = "passed"
+      else if (this.props.value[key]['now_certificated_credit_num'] + this.props.value[key]['feature_certificated_credit_num'] >= this.props.value[key]['min_certificated_credit_num'])
+        passed = "expect"
+      else
+        passed = "failed"
+      itemList.push([key, pushStr, passed])
     }
+    if (this.props.depth === 0) {
+      return (
+        <div className='con'>
+          {itemList.map(([key, st, passed]) => (
+            <div className={'bl_tate ' + passed} key={key + st}>
+              <p className='hight-center last-leaf'>
+                {(key === "" ? "" : key) + st}
+              </p>
+            </div>
+          ))}
+        </div>
+      )
+    }
+
+    return (
+      <div className='con'>
+        {itemList.map(([key, st, passed]) => (
+          <div className={'gre_yoko ' + passed} key={key + st}>
+            <div className={'bl_tate ' + passed} key={key + st + 'top'}>
+              <p className='hight-center'>
+                {(key === "" ? "" : key)}
+              </p>
+              <p className='hight-center'>
+                {st}
+              </p>
+            </div>
+            <div className={'bl_tate2 ' + passed} key={key + st}>
+              <Table value={this.props.value[key]['leaf']} depth={this.props.depth - 1} />
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 function Main() {
-    let [JsonData, setJsonData] = useState(Data);
+  let [JsonData, setJsonData] = useState(Data);
+  let [GradeData, setGradeData] = useState();
+  const [, rerender] = useState();
 
-    const onFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files[0];
-        const reader = new FileReader();
-        reader.onload = () => {
-            setJsonData(JSON.parse(reader.result));
-        };
-        reader.readAsText(file);
+  const onFileInputChangeCSV = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      // setGradeData(readCsv(reader.result));
+      checkGraduate(readCsv(reader.result), JsonData, (res) => {
+        setJsonData(res);
+      });
     };
+    reader.readAsText(file);
+  };
 
-    return (
-        <div className="App">
-            <header className="App-Upload">
-                <div>
-                    <input type="file" onChange={onFileInputChange} />
-                </div>
-            </header>
-            <header className="App-header">
-                <Table value={JsonData} depth={4} />
-            </header>
+  const onFileInputChangeJSON = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      setJsonData(JSON.parse(reader.result));
+    };
+    reader.readAsText(file);
+  };
+  const selectReq = (f) => {
+    // console.log(f.value);
+    var data = require('./reqJson/' + f.value);
+    // console.log(data);
+    setJsonData(data);
+    // const file = e.target.files[0];
+    // const reader = new FileReader();
+    // reader.onload = () => {
+    //   setJsonData(JSON.parse(reader.result));
+    // };
+    // reader.readAsText(file);
+  };
+
+  const update = () => {
+    console.log(GradeData);
+  }
+  const update2 = () => {
+    console.log(JsonData)
+  }
+  const update3 = () => {
+    checkGraduate(GradeData, JsonData, (res) => {
+      setJsonData(res);
+    });
+  }
+  const options = [
+    { value: 'mast20.json', label: 'mast20' },
+    { value: 'coins19.json', label: 'coins19' },
+    { value: 'coins20.json', label: 'coins20' },
+    { value: 'coins21.json', label: 'coins21' }
+  ]
+
+  return (
+    <div className="App">
+      <header className="App-Upload">
+        <div>
+          <a>
+            target csv
+          </a>
+          <input type="file" onChange={onFileInputChangeCSV} />
+          {/* </div> */}
+          {/* <div>
+          <a>
+            requirements json
+          </a>
+          <input type="file" onChange={onFileInputChangeJSON} />
+        </div> */}
+          {/* <div> */}
+          {/* <button onClick={update}>
+            check csv
+          </button>
+          <button onClick={update2}>
+            check json
+          </button>
+          <button onClick={update3}>
+            update
+          </button> */}
+          {/* </div> */}
+          {/* <div> */}
+          <Select
+            options={options}
+            onChange={selectReq}
+            defaultValue={{ value: 'coins20.json', label: 'coins20' }}
+          />
+          <button onClick={rerender}>
+            rerender
+          </button>
         </div>
-    );
+      </header>
+      <header className="App-header">
+        <Table value={JsonData} depth={4} />
+      </header>
+    </div>
+  );
 }
 
 function App() {
-    return (
-        <Main />
-    );
+  return (
+    <Main />
+  );
 }
 
 export default App;
---
+++ src/checkGraduate/checkGrad.ts
@@ -0,0 +1,124 @@
+
+const max = 'max_certificated_credit_num';
+const now = 'now_certificated_credit_num';
+const min = 'min_certificated_credit_num';
+const feature = 'feature_certificated_credit_num';
+export function checkGraduate(grade, req, callback) {
+  // console.log(req);
+  // console.log(grade);
+  for (let rishu = 0; rishu < 2; rishu++)
+    for (const k1 in req) {
+      req[k1] = genNow(
+        req[k1]
+      );
+      for (const k2 in req[k1]['leaf']) {
+        req[k1]['leaf'][k2] = genNow(
+          req[k1]['leaf'][k2]
+        );
+        for (const k3 in req[k1]['leaf'][k2]['leaf']) {
+          req[k1]['leaf'][k2]['leaf'][k3] = genNow(
+            req[k1]['leaf'][k2]['leaf'][k3]
+          );
+          for (const k4 in req[k1]['leaf'][k2]['leaf'][k3]['leaf']) {
+            req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4] = genNow(
+              req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4]
+            );
+            for (const k5 in req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4]['leaf']) {
+              req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4]['leaf'][k5] = genNow(
+                req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4]['leaf'][k5]
+              );
+              req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4]['leaf'][k5] = checkGet(
+                grade,
+                req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4]['leaf'][k5],
+                rishu === 1
+              );
+              req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4][now]
+                += rishu * req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4]['leaf'][k5][now];
+              req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4][feature]
+                += rishu * req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4]['leaf'][k5][feature];
+            }
+            req[k1]['leaf'][k2]['leaf'][k3][now]
+              += rishu * req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4][now];
+            req[k1]['leaf'][k2]['leaf'][k3][feature]
+              += rishu * req[k1]['leaf'][k2]['leaf'][k3]['leaf'][k4][feature];
+          }
+          req[k1]['leaf'][k2][now]
+            += rishu * req[k1]['leaf'][k2]['leaf'][k3][now];
+          req[k1]['leaf'][k2][feature]
+            += rishu * req[k1]['leaf'][k2]['leaf'][k3][feature];
+        }
+        req[k1][now]
+          += rishu * req[k1]['leaf'][k2][now];
+        req[k1][feature]
+          += rishu * req[k1]['leaf'][k2][feature];
+      }
+    }
+
+  return req;
+}
+
+function genNow(req) {
+  if (req[now] === undefined) {
+    req[now] = 0;
+    req[feature] = 0;
+  }
+  return req;
+}
+
+function checkGet(grade, req, rishu) {
+  // req[now] += 10;
+  // console.log(req);
+  const reg_name = req['leaf']['regexp_name'];
+  const reg_number = req['leaf']['regexp_number'];
+  // console.log(reg_name)
+  // console.log(reg_number)
+  for (const key in grade) {
+    let flag = false;
+    if (reg_name !== "") {
+      const reg = new RegExp(reg_name);
+      if (grade[key]["course_name"].match(reg)) {
+        flag = true;
+      }
+      // console.log(":")
+    }
+    if (reg_number !== "") {
+      const reg = new RegExp(reg_number);
+      if (grade[key]["course_number"].match(reg)) {
+        flag = true;
+      }
+      // console.log(":")
+    }
+    if (flag) {
+      //     if not kamoku.used:
+      // console.log("---" + reg_name + "regregreg" + reg_number);
+      // console.log("---" + grade[key]["course_name"] + "/" + grade[key]["credit"]);
+      if (grade[key]["used"] === false && grade[key]["used_rishu"] === false) {
+        if (grade[key]["can_use"] === true) {
+          grade[key]["used"] = true;
+          req[now] += grade[key]["credit"];
+          // req[now] += grade[key]["credit"];
+          // req[now] += grade[key]["credit"];
+        } else if (grade[key]["grade"] === "履修中" && rishu) {
+          grade[key]["used_rishu"] = true;
+          req[feature] += grade[key]["credit"];
+          console.log(rishu);
+          console.log(grade[key]["course_name"]);
+        }
+      }
+
+      //         if kamoku.can_use:
+      //             kamoku.used = True
+      //             res_credit += kamoku.credit
+      //             res_course_name.append("{}{}{}".format(Color.GREEN, kamoku.course_name, Color.RESET))
+      //         elif kamoku.grade == "履修中":
+      //             kamoku.used = True
+      //             feature_credit += kamoku.credit
+      //             res_course_name.append("{}{}{}".format(FEATURE_COLOR_STR, kamoku.course_name, Color.RESET))
+      //         else:
+      //             if args.drop:
+      //                 res_course_name.append("{}{}{}".format(Color.RED, kamoku.course_name, Color.RESET))
+    }
+
+  }
+  return req;
+}
