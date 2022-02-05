8d859c07f553d5dc987ab9139b5535e43f333f6d
oshamashama <65126083+oshamashama@users.noreply.github.com>
2022-02-05 19:26:19 +0900

    add shellscript

---
+++ src/reqJson/genJson.sh
@@ -0,0 +1,8 @@
+yq . cis20.yml | jq --indent 4 '' > cis20.json
+yq . coins19.yml | jq --indent 4 '' > coins19.json
+yq . coins20.yml | jq --indent 4 '' > coins20.json
+yq . coins21.yml | jq --indent 4 '' > coins21.json
+yq . mast20.yml | jq --indent 4 '' > mast20.json
+yq . math20.yml | jq --indent 4 '' > math20.json
+yq . psych20.yml | jq --indent 4 '' > psych20.json
+
---
+++ src/reqJson/genYml.sh
@@ -0,0 +1,8 @@
+yq -y . cis20.json > cis20.yml
+yq -y . coins19.json > coins19.yml
+yq -y . coins20.json > coins20.yml
+yq -y . coins21.json > coins21.yml
+yq -y . mast20.json > mast20.yml
+yq -y . math20.json > math20.yml
+yq -y . psych20.json > psych20.yml
+
