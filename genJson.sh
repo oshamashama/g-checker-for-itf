31b0bd7cc4ac5abbe89b383b9f0a56be0de5ebe6
oshamashama <65126083+oshamashama@users.noreply.github.com>
2022-04-12 00:52:06 +0900

    merge: main->testgithubactions

--- genJson.sh
+++ genJson.sh
@@ -1,10 +1,10 @@
 rm src/reqJson/*
-yq . reqYml/cisIR20.yml | jq --indent 4 '' > src/reqJson/cisIR20.json
-yq . reqYml/cisID20.yml | jq --indent 4 '' > src/reqJson/cisID20.json
-yq . reqYml/coins19.yml | jq --indent 4 '' > src/reqJson/coins19.json
-yq . reqYml/coins20.yml | jq --indent 4 '' > src/reqJson/coins20.json
-yq . reqYml/coins21.yml | jq --indent 4 '' > src/reqJson/coins21.json
-yq . reqYml/mast20.yml | jq --indent 4 '' > src/reqJson/mast20.json
-yq . reqYml/math20.yml | jq --indent 4 '' > src/reqJson/math20.json
-yq . reqYml/psych20.yml | jq --indent 4 '' > src/reqJson/psych20.json
+yq -o json . reqYml/cisIR20.yml | jq --indent 4 '' > src/reqJson/cisIR20.json
+yq -o json . reqYml/cisID20.yml | jq --indent 4 '' > src/reqJson/cisID20.json
+yq -o json . reqYml/coins19.yml | jq --indent 4 '' > src/reqJson/coins19.json
+yq -o json . reqYml/coins20.yml | jq --indent 4 '' > src/reqJson/coins20.json
+yq -o json . reqYml/coins21.yml | jq --indent 4 '' > src/reqJson/coins21.json
+yq -o json . reqYml/mast20.yml | jq --indent 4 '' > src/reqJson/mast20.json
+yq -o json . reqYml/math20.yml | jq --indent 4 '' > src/reqJson/math20.json
+yq -o json . reqYml/psych20.yml | jq --indent 4 '' > src/reqJson/psych20.json
 
