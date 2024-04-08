f919797e1f2f16a36dbc099726ee3b1fa34131c7
WhatACotton <118359411+WhatACotton@users.noreply.github.com>
2024-04-08 18:27:05 +0900

    coins23生用の履修要件の追加 (#14)
    * add: update for coins23
    
    * add: update name
    
    * add: for coins24
    
    * fix: rename file
    
    * Update react-visualizer/src/App.js
    
    Co-authored-by: shama <uwyiqmq@gmail.com>
    
    * Update react-visualizer/src/reqJson/coins23-24.json
    
    Co-authored-by: shama <uwyiqmq@gmail.com>
    
    * Update react-visualizer/src/reqJson/coins23-24.json
    
    Co-authored-by: shama <uwyiqmq@gmail.com>
    
    * Update react-visualizer/src/reqJson/coins23-24.json
    
    Co-authored-by: shama <uwyiqmq@gmail.com>
    
    ---------
    
    Co-authored-by: shama <uwyiqmq@gmail.com>

--- react-visualizer/src/App.js
+++ react-visualizer/src/App.js
@@ -167,6 +167,8 @@ function Main() {
     { value: "coins19.json", label: "coins19" },
     { value: "coins20.json", label: "coins20" },
     { value: "coins21.json", label: "coins21" },
+    { value: "coins23-24.json", label: "coins23" },
+    { value: "coins23-24.json", label: "coins24" },
     { value: "klis21-system.json", label: "klis21(知識情報システム)" },
     { value: "klis21-knowledge.json", label: "klis21(知識科学)" },
     { value: "klis21-management.json", label: "klis21(情報資源経営)" },
