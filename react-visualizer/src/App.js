a1d68973f4f9821396ff484687258925cfa329b4
whatacotton <whatacotton@gmail.com>
2024-04-08 15:48:38 +0900

    add: update name

--- react-visualizer/src/App.js
+++ react-visualizer/src/App.js
@@ -167,7 +167,7 @@ function Main() {
     { value: "coins19.json", label: "coins19" },
     { value: "coins20.json", label: "coins20" },
     { value: "coins21.json", label: "coins21" },
-    { value: "klis23.json", label: "klis23" },
+    { value: "coins23.json", label: "coins23" },
     { value: "klis21-system.json", label: "klis21(知識情報システム)" },
     { value: "klis21-knowledge.json", label: "klis21(知識科学)" },
     { value: "klis21-management.json", label: "klis21(情報資源経営)" },
